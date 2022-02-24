d727332c47d0a726995184a513dccff3aa42e264
Marcus <marcuscoelho07@gmail.com>
2022-02-24 17:20:41 -0300

    git status

---
+++ tutorial_2/App_tutorial_2/App_tutorial_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Character escape sequences and textual strings tutorial
+
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("My \nName \nis \nMarcus");
+Console.WriteLine("Hi\nMyn friend!");
+
+Console.WriteLine("tab1 \ttab 2 \t tab3 \t tab4");
+
+// just to see the behavior of use \n and \t inside strings
+// the \n sequence will add a new line, and a \t sequence will add a tab.
+
+// other exemples
+
+// this is to use double-quotation  (") insert a \"
+Console.WriteLine("Hello \"World\"!");
