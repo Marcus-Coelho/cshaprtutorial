28254fa3f2ed18b9f285d2b1391b06e133afa2cc
Marcus-Coelho <marcuscoelho07@gmail.com>
2022-03-01 18:01:33 -0300

    add uploded tutorial 4 and others

---
+++ tutorial_3/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string? Name { get; set; } //? is to do Name as possible null.
+        public int Age { get; set; } //case null value, 0 is the output
+
+        public void IntroduceYourSelf()
+        {
+            Console.WriteLine($"Hi! My name is {Name} and I'm {Age}"); // $ is to allow interpolation
+        }
+
+    }
+}
---
+++ tutorial_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleApp1;
+
+class Programa
+{
+
+    static void Main(String[] args)
+    {
+        Person p1 = new Person(); // o comando new chama para abrir uma instancia (um objeto) que ficará alocado em p1.
+                                    // command call to open a instance (called object) and it will be alocaded at p1.
+
+        p1.Name = "Amada"; //nesse momento está sendo atribuído o valor de "Amada" para a propriedade Nome ao objeto p1.
+                            // at this moment "Amada" is a property Name of p1 object
+        p1.Age = 38;        //this time 38 is a property Age of p1 object
+
+        p1.IntroduceYourSelf(); // call return at Person class
+                
+           }
+}
